5ad949e99a2f90f8afb0e12f6e48b86c2315a7dc
s0mg2703 <32416650+s0mg2703@users.noreply.github.com>
2020-03-02 04:26:31 +0700

    OutputModel for create, update and delete keyword of user

---
+++ src/main/java/fpt/capstone/betatest/model/MessageOutputModel.java
@@ -0,0 +1,26 @@
+package fpt.capstone.betatest.model;
+
+public class MessageOutputModel {
+	int statusCode;
+	String statusMessage;
+	
+	public MessageOutputModel() {
+	}
+
+	public int getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(int statusCode) {
+		this.statusCode = statusCode;
+	}
+
+	public String getStatusMessage() {
+		return statusMessage;
+	}
+
+	public void setStatusMessage(String statusMessage) {
+		this.statusMessage = statusMessage;
+	}
+
+}
