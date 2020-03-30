82cb91099cb70373f380977fbfdba28d87aee9d8
Trung <letrung1998vn@gmail.com>
2020-03-30 17:05:36 +0700

    Update PostRepository.java

--- src/main/java/fpt/capstone/betatest/repositories/PostRepository.java
+++ src/main/java/fpt/capstone/betatest/repositories/PostRepository.java
@@ -11,14 +11,12 @@ import fpt.capstone.betatest.entities.Post;
 
 @Repository("postRepository")
 public interface PostRepository extends JpaRepository<Post, Integer> {
-	@Query(nativeQuery = true, value = "select * from Post s1 "
-			+ "JOIN ( select post_content, MAX(crawl_date) as date from Post GROUP BY post_content) s2 "
-			+ "on s1.post_content=s2.post_content " + "and s1.crawl_date=s2.date where keyword=?1")
+	@Query(nativeQuery = true, value = "select * from Post p,"
+			+ " (select post_content, MAX(crawl_date) as date from Post GROUP BY post_content) w "
+			+ "where p.post_content=w.post_content and p.crawl_date=w.date and keyword=?1")
 	List<Post> getEachPostContentWithLatestDate(String keyword);
 
-	@Query(nativeQuery = true, value = "select * from Post s1 "
-			+ "JOIN ( select post_content, MAX(crawl_date) as date from Post GROUP BY post_content) s2 "
-			+ "on s1.post_content=s2.post_content where keyword=?1")
+	@Query(nativeQuery = true, value = "select * from Post where keyword=?1 order by post_content")
 	List<Post> getPostContentWithTwoLatestDate(String keyword);
 	
 	List<Post> getByPostId(BigInteger id);
